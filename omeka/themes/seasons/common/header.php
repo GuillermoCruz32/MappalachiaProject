67b65aec938efd99374f526cc8f6d568e4191022
Elaheh Jamali <ejamali@Elas-MacBook-Pro.local>
2020-10-26 19:16:48 -0400

    trying the favicon

--- omeka/themes/seasons/common/header.php
+++ omeka/themes/seasons/common/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:image" content="/favicon.png">
     <?php if ($description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>">
     <?php endif; ?>
